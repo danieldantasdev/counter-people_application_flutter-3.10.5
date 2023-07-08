429fcf8d6a7edf5ab92ff2d0c5ca564bb5d7a49b
danieldantasdev <contatolobinhodev@gmail.com>
2023-07-08 16:45:59 -0300

    bugfix: line cut

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';mport 'package:foodhub/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:foodhub/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
